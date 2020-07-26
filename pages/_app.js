c43698bb5654676148639501f914422036861f21
pradeep.thoma <pradeep.thoma@compassitesinc.com>
2020-07-26 21:10:48 +0530

    Moved Layer context to Map so that it gets destroyed when navigated away from map

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,15 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
-import LayerContextProvider from '../context/Layer';
 
 import hhmTheme from './theme';
 import '../styles/core.scss';
 
 export default ({ Component, pageProps }) => {
 	return (
-		<LayerContextProvider>
-			<ThemeProvider theme={hhmTheme}>
-				<ColorModeProvider>
-					<CSSReset />
-					<Component {...pageProps} />
-				</ColorModeProvider>
-			</ThemeProvider>
-		</LayerContextProvider>
+		<ThemeProvider theme={hhmTheme}>
+			<ColorModeProvider>
+				<CSSReset />
+				<Component {...pageProps} />
+			</ColorModeProvider>
+		</ThemeProvider>
 	);
 };
--- pages/map/index.js
+++ pages/map/index.js
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 import _ from 'underscore';
 import { Grid, Box, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/core';
 
+import LayerContextProvider from '../../context/Layer';
 import Sidebar from '../../components/Sidebar';
 import Map from '../../components/Map';
 import DataGrid from '../../components/DataGrid';
@@ -12,48 +13,50 @@ import { LayerContext } from '../../context/Layer';
 
 export function HomePage({ username, loading, error, repos, onSubmitForm, onChangeUsername }) {
 	// const [selectedTabIndex, setSelectedTabIndex] = useState(0);
-	const { currentIndicatorData } = useContext(LayerContext);
+	// const { currentIndicatorData } = useContext(LayerContext);
 
 	return (
-		<Layout>
-			<article className="main-container">
-				<Helmet>
-					<title>Home Page</title>
-					<meta name="description" content="Health Heat Map" />
-				</Helmet>
-				<div>
-					<Grid gridTemplateColumns={'30% 1fr'} gap={0}>
-						<Box>
-							<Sidebar />
-						</Box>
+		<LayerContextProvider>
+			<Layout>
+				<article className="main-container">
+					<Helmet>
+						<title>Home Page</title>
+						<meta name="description" content="Health Heat Map" />
+					</Helmet>
+					<div>
+						<Grid gridTemplateColumns={'30% 1fr'} gap={0}>
+							<Box>
+								<Sidebar />
+							</Box>
 
-						<Box className="vis-right-column">
-							<div className="visualization-area">
-								{/* Visualization Area */}
-								<Tabs>
-									<TabList>
-										<Tab>Map</Tab>
-										{!_.isEmpty(currentIndicatorData) && <Tab>Table</Tab>}
-									</TabList>
+							<Box className="vis-right-column">
+								<div className="visualization-area">
+									{/* Visualization Area */}
+									<Tabs>
+										<TabList>
+											<Tab>Map</Tab>
+											{/* {!_.isEmpty(currentIndicatorData) && <Tab>Table</Tab>} */}
+										</TabList>
 
-									<TabPanels>
-										<TabPanel>
-											{/* Map Component */}
-											<Map />
-										</TabPanel>
-										<TabPanel>
-											{!_.isEmpty(currentIndicatorData) && (
-												<DataGrid IndicatorData={currentIndicatorData} />
-											)}
-										</TabPanel>
-									</TabPanels>
-								</Tabs>
-							</div>
-						</Box>
-					</Grid>
-				</div>
-			</article>
-		</Layout>
+										<TabPanels>
+											<TabPanel>
+												{/* Map Component */}
+												<Map />
+											</TabPanel>
+											<TabPanel>
+												{/* {!_.isEmpty(currentIndicatorData) && (
+													<DataGrid IndicatorData={currentIndicatorData} />
+												)} */}
+											</TabPanel>
+										</TabPanels>
+									</Tabs>
+								</div>
+							</Box>
+						</Grid>
+					</div>
+				</article>
+			</Layout>
+		</LayerContextProvider>
 	);
 }
 
