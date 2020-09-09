b54429cab954fc8f0a944c92bfbd76421c26ce2b
pradeep.thoma <pradeep.thoma@compassitesinc.com>
2020-09-09 17:33:29 +0530

    fix: Kottayam color issue - more generic fix due to array indexing in map data formatter

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -27,7 +27,7 @@ const Header = (props) => {
 				<Flex align="center" mr={5}>
 					<Heading as="h1" size="lg" letterSpacing={'-.05rem'}>
 						<Link href="/">
-							<a href="#">Health Heatmap <sup className="title-sup">Beta</sup></a>
+							<a href="#">Health Heatmap <sup className="title-sup">alpha</sup></a>
 						</Link>
 					</Heading>
 				</Flex>
