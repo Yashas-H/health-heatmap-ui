54deb5f29a5767880eb49a1e116c7db645200ef7
Akshay S Dinesh <asdofindia@gmail.com>
2020-09-28 15:12:32 +0530

    feat: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+
+import * as gtag from '../lib/gtag'
 
 import hhmTheme from '../styles/theme';
 import '../styles/core.scss';
 
 export default ({ Component, pageProps }) => {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
 	return (
 		<ThemeProvider theme={hhmTheme}>
 				<CSSReset />
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class HTMLDocument extends Document {
 	static async getInitialProps(ctx) {
@@ -14,6 +15,25 @@ class HTMLDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
 						rel="stylesheet"
 					></link>
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', '${GA_TRACKING_ID}', {
+									page_path: window.location.pathname,
+								});
+								if (window.location.hostname === 'localhost') { // disable GA:
+									window['ga-disable-${GA_TRACKING_ID}'] = true; // enter your tracking ID
+								}
+							`,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
